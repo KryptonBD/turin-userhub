d052cd3b9ddea13a769eb5aa1b7a30bd3db52ded
KryptonBD <foysol.sahi@gmail.com>
2024-11-23 15:14:06 +0000

    feat: Integrating User list and fixing angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(withInterceptors([ErrorInterceptor])),
+    provideHttpClient(withInterceptors([ErrorInterceptor])), provideAnimationsAsync(),
   ],
 };
--- src/app/features/users/components/user-list/user-list.component.ts
+++ src/app/features/users/components/user-list/user-list.component.ts
@@ -1,19 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { UserComponent } from '../user/user.component';
+import { UserService } from '@app/core/services/user.service';
+import { User } from '@app/core/models/user.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
   templateUrl: 'user-list.component.html',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, MatPaginatorModule],
 })
 export class UserListComponent {
-  users = Array.from({ length: 10 }, (_, i) => ({
-    id: i + 1,
-    first_name: `John ${i}`,
-    last_name: `Doe`,
-    email: `john${i}@test.com`,
-    avatar: 'https://picsum.photos/200',
+  private userService = inject(UserService);
+  private destroyRef = inject(DestroyRef);
+
+  protected users = signal<User[]>([]);
+  protected pageSize = signal(5);
+  protected currentPage = signal(1);
+  protected totalUsers = signal(0);
+
+  private paginator = computed(() => ({
+    page: this.currentPage(),
+    perPage: this.pageSize(),
   }));
+
+  constructor() {
+    effect(() => {
+      this.loadUsers();
+    });
+  }
+
+  // Load Users
+  private loadUsers() {
+    const { page, perPage } = this.paginator();
+    this.userService
+      .getUsers(page, perPage)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((response) => {
+        this.users.set(response.data);
+        this.totalUsers.set(response.total);
+      });
+  }
+
+  protected pageChange(event: PageEvent) {
+    console.log(event);
+    this.pageSize.set(event.pageSize);
+    this.currentPage.set(event.pageIndex + 1);
+  }
 }
